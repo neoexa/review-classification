19e1d9722a9650c172925bae156b596f4460d36f
Nabab <arthurizard@gmail.com>
2018-04-26 20:19:11 +0200

    updateReadMe

---
+++ scripts/build_package.py
@@ -0,0 +1,14 @@
+import os
+
+print('_____INSTALL PACKAGE_____')
+
+os.system('sudo apt install python-pip')
+os.system('sudo python -m pip install sklearn')
+os.system('sudo python -m pip install Numpy')
+os.system('sudo python -m pip install pandas')
+os.system('sudo python -m pip install scipy')
+os.system('sudo python -m pip install matplotlib')
+os.system('sudo python -m pip install seaborn')
+
+
+print('_____DONE_____')
